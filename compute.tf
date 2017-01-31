818cc54e94e93d45cad9fbd38a1b1eb3424a40d5
Patrick Lecuyer <pl@conseilpatricklecuyer.com>
2017-01-30 22:10:07 -0500

    Added cloud-config

--- compute.tf
+++ compute.tf
@@ -9,12 +9,6 @@ resource "google_compute_instance" "master" {
    image = "coreos-beta-1235-1-0-v20161130"
   }
 
- disk {
-   disk = "${google_compute_disk.vault-db.name}"
-   device_name = "vaultdb"
-   auto_delete = false
- }
-
 
 network_interface {
    subnetwork = "${google_compute_subnetwork.default-us-east1.name}"
@@ -23,11 +17,3 @@ network_interface {
    }
  }
 }
-
- resource "google_compute_disk" "vault-db" {
-   name  = "vault-db"
-   type  = "pd-standard"
-   zone  = "us-east1-c"
-   size = 10
-
- }
